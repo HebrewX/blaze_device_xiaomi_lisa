864dd5831fcc4797362dbadc4d79b8f97fdcf170
HebrewX <141772036+HebrewX@users.noreply.github.com>
2023-09-25 09:05:45 +0530

    Update and rename derp_lisa.mk to blaze_lisa.mk

--- blaze_lisa.mk
+++ blaze_lisa.mk
@@ -12,13 +12,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
 # Inherit some common PixelOS stuff
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lisa
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := derp_lisa
+PRODUCT_NAME := blaze_lisa
 PRODUCT_MODEL := Xiaomi 11 Lite NE
+
+# Project Blaze Stuff
+BLAZE_BUILD_TYPE := OFFICIAL
+BLAZE_MAINTAINER := ん 乇 乃 尺 乇 W ﾒ
+TARGET_SUPPORTS_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_FACE_UNLOCK := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
