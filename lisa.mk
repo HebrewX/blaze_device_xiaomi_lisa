97d793ca27fbc0a34cc10b620b34b12a4b309567
HebrewX <141772036+HebrewX@users.noreply.github.com>
2023-11-07 07:37:33 +0530

    Update lisa.mk

--- lisa.mk
+++ lisa.mk
@@ -12,20 +12,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
 # Inherit some common PixelOS stuff
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lisa
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := blaze_lisa
+PRODUCT_NAME := lisa
 PRODUCT_MODEL := Xiaomi 11 Lite NE
 
 # Project Blaze Stuff
-BLAZE_BUILD_TYPE := OFFICIAL
-BLAZE_MAINTAINER := 卄乇乃尺乇山乂
-TARGET_SUPPORTS_BLUR := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_FACE_UNLOCK := true
+CUSTOM_BUILD_TYPE := Official
+SHIP_APERTURE := true
+GMS_FULL := true
+WITH_GMS := true
+USE_PIXEL_CHARGER_IMAGES := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
